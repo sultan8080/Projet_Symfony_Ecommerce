4a005dbcf1235682d4e1de1a6e574ebaf7dcc3d6
Moti-Sultan <motisultannur@gmail.com>
2023-03-29 13:40:32 +0200

    BUG data fixture Solved

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -13,18 +13,49 @@ class CategoryFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $faker = Faker\Factory::create('fr_FR');
-        $category = new Category();
-        $category->setNom($faker->word());
-        $manager->persist($category);
+
+        $categoryProduits  = [
+            1 => [
+                'nom' => 'Legumes',
+                'slug' => 'Legumes',
+            ],
+            2 => [
+                'nom' => 'Epicerie',
+                'slug' => 'Epicerie',
+            ],
+
+            3 => [
+                'nom' => 'Fruits',
+                'slug' => 'Fruits',
+            ],
+
+            4 => [
+                'nom' => 'Boissons',
+                'slug' => 'Boissons',
+            ],
+
+        ];
+
+        foreach ($categoryProduits as $key => $value) {
+            $category = new Category();
+            $category->setNom($value['nom']);
+            $category->setSlug($value['slug']);
+            $manager->persist($category);
+            $this->addReference('category-' . $key, $category);
+        }
+
+        // $faker = Faker\Factory::create('fr_FR');
+        // $category = new Category();
+        // $category->setNom($faker->word());
+        // $manager->persist($category);
 
 
-        $category1 = new Category();
-        $category1->setNom($faker->word());
-        $manager->persist($category1);
+        // $category1 = new Category();
+        // $category1->setNom($faker->word());
+        // $manager->persist($category1);
 
-        $this->addReference('category', $category);
-        $this->addReference('category1', $category1);
+        // $this->addReference('category', $category);
+        // $this->addReference('category1', $category1);
 
         $manager->flush();
     }
--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -15,25 +15,20 @@ class ProductFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Faker\Factory::create('fr_FR');
-        $product = new Product();
-        $product->setNom($faker->word());
-        $product->setPrix($faker->randomFloat(2));
-        $product->setCreatedAt($faker->dateTime());
 
-        $manager->persist($product);
-
-
-
-        $product1 = new Product();
-        $product1->setNom($faker->word());
-        $product1->setPrix($faker->randomFloat(2));
-        $product1->setCreatedAt($faker->dateTime());
-
-        $product->setCategory($this->getReference('category'));
-        $product1->setCategory($this->getReference('category1'));
-
-        $manager->persist($product1);
-
+        for ($i = 0; $i < 50; $i++) {
+            $product = new Product();
+            $product->setNom($faker->word);
+            $product->setPrix($faker->randomFloat(2));
+            $product->setSlug($faker->slug());
+            $product->setCreatedAt($faker->dateTime());
+            $product->setOnline(true);
+            $product->setImage($faker->imageUrl(640, 480, 'animals', true));
+            $product->setDescription($faker->text(100));
+            $product->setSousTitre($faker->sentence());
+            $product->setCategory($this->getReference('category-' . $faker->numberBetween(1, 4)));
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
