9f8c28dc4262c73da863d2d28f156d14cc141814
Moti-Sultan <motisultannur@gmail.com>
2023-03-30 22:46:09 +0200

    All Product Lists created

---
+++ src/Form/ProductType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('Prix')
+            ->add('createdAt')
+            ->add('slug')
+            ->add('image')
+            ->add('online')
+            ->add('description')
+            ->add('sousTitre')
+            ->add('updatedAt')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
