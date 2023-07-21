c29d9b8c4917ab2636cf011e0518faec43b7405b
Moti-Sultan <motisultannur@gmail.com>
2023-07-21 11:47:22 +0200

    User Data Fixtures Modified with password hasher

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -7,9 +7,19 @@ use App\Entity\User;
 use DateTime;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+
+
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Faker\Factory::create('fr_FR');
@@ -18,9 +28,18 @@ class UserFixtures extends Fixture
             $user = new User();
             $user->setNom($faker->word);
             $user->setEmail($faker->email);
-            $user->setPassword($faker->password());
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, '123456'));
             $user->setRoles(['ROLE_USER']);
             $manager->persist($user);
+        }
+
+        for ($i = 0; $i < 2; $i++) {
+            $admin = new User();
+            $admin->setNom($faker->word);
+            $admin->setEmail($faker->email);
+            $admin->setPassword($this->userPasswordHasher->hashPassword($admin, '123456'));
+            $admin->setRoles(['ROLE_ADMIN']);
+            $manager->persist($admin);
         }
         $manager->flush();
     }
